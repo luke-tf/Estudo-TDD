75654949b240e422ea026ecbf5030c28557ab334
luke-tf <lukero.toledo@gmail.com>
2020-12-17 15:56:03 -0300

    Modulo 3 - Melhorando os testes

---
+++ test/CursoOnline.DominioTest/Cursos/CursoTest.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using CursoOnline.DominioTest._Builders;
+using CursoOnline.DominioTest._util;
+using ExpectedObjects;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CursoOnline.DominioTest.Cursos
+{
+    public class CursoTest : IDisposable
+    {
+        private readonly ITestOutputHelper _output;
+        private readonly string _nome;
+        private readonly double _cargaHoraria;
+        private readonly PublicoAlvo _publicoAlvo;
+        private readonly double _valor;
+        private readonly string _descricao;
+
+        public CursoTest(ITestOutputHelper output)
+        {
+            _output = output;
+            _output.WriteLine("Construtor sendo executado");
+
+            _nome = "Informática Básica";
+            _cargaHoraria = 80;
+            _publicoAlvo = PublicoAlvo.Estudante;
+            _valor = 950;
+            _descricao = "Uma descrição";
+        }
+
+        public void Dispose()
+        {
+            _output.WriteLine("Dispose sendo executado");
+        }
+
+        [Fact]
+        public void DeveCriarCurso()
+        {
+            var cursoEsperado = new
+            {
+                Nome = _nome,
+                CargaHoraria = _cargaHoraria,
+                PublicoAlvo = _publicoAlvo,
+                Valor = _valor
+            };
+
+            var curso = new Curso(cursoEsperado.Nome,cursoEsperado.CargaHoraria,cursoEsperado.PublicoAlvo,cursoEsperado.Valor, _descricao);
+
+            cursoEsperado.ToExpectedObject().ShouldMatch(curso);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void CursoNaoDeveTerUmNomeInvalido(string nomeInvalido)
+        {
+            Assert.Throws<ArgumentException>(() =>
+                CursoBuilder.Novo().ComNome(nomeInvalido).Build())
+                .ComMensagem("Nome inválido");
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-2)]
+        [InlineData(-100)]
+        public void CursoNaoDeveTerCargaHorariaMenorQue1(double cargaHorariaInvalida)
+        {
+            Assert.Throws<ArgumentException>(() =>
+                    CursoBuilder.Novo().ComCargaHoraria(cargaHorariaInvalida).Build())
+                    .ComMensagem("Carga horária inválida");
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-2)]
+        [InlineData(-100)]
+        public void CursoNaoDeveTerUmValorMenorQue1(double valorInvalido)
+        {
+            var cursoEsperado = new
+            {
+                Nome = "Informática Básica",
+                CargaHoraria = (double)80,
+                PublicoAlvo = PublicoAlvo.Estudante,
+                Valor = (double)950
+            };
+
+            Assert.Throws<ArgumentException>(() =>
+                CursoBuilder.Novo().ComValor(valorInvalido).Build())
+                .ComMensagem("Valor inválido");
+        }
+    }
+
+    public enum PublicoAlvo
+    {
+        Estudante,
+        Universitario,
+        Empregado,
+        Empreendedor
+    }
+    
+
+    public class Curso
+    {
+        public Curso(string nome, double cargaHoraria, PublicoAlvo publicoAlvo, double valor,string descricao)
+        {
+            if (string.IsNullOrEmpty(nome))
+                throw new ArgumentException("Nome inválido");
+
+            if (cargaHoraria < 1)
+                throw new ArgumentException("Carga horária inválida");
+
+            if (valor < 1)
+                throw new ArgumentException("Valor inválido");
+            
+            Nome = nome;
+            CargaHoraria = cargaHoraria;
+            PublicoAlvo = publicoAlvo;
+            Valor = valor;
+            Descricao = descricao;
+        }
+
+        public string Nome { get; private set; }
+        public double CargaHoraria { get; private set; }
+        public PublicoAlvo PublicoAlvo { get; private set; }
+        public double Valor { get; private set; }
+        public string Descricao { get; private set; }
+    }
+}
